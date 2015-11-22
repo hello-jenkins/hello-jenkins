373d2c379b6690f70cc66ca03b613216dd285374
Hello Jenkins <hello-jenkins@hulahula.com>
2015-11-22 21:27:25 +0000

    Test respond

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
  
 describe('GET /', function() {
-  it('talks bla-bla-bla with hello world', function(done) {
+  it('respond with hello world', function(done) {
     request(app).get('/').expect('hello world', done);
   });
 });
